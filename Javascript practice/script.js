719fe49b8e19dcef0317053a69eb6df86c016917
JimXu <zxu01@risd.edu>
2019-01-16 09:16:58 -0500

    Update script.js

--- Javascript practice/script.js
+++ Javascript practice/script.js
@@ -1,51 +1,10 @@
-
-var date= new Date();
-document.body.innerHTML 
-= "<p>Today is:"+date+"</p>";
-
-/**/
-
-var a;
-var b;
-var sum =a+b;
-var theNumbersMatch;
-
-
-if (a==b){
-	theNumbersMatch = true;
-} else {
-	theNumbersMatch = false;
+function findBiggestFraction(){
+	var result;
+	a>b ? result=["first fraction:",a] : result= ["second fraction:", b]
+	return result;
 }
 
-//The same as the Ternery operator below;
-// a==b? console.log("Match"):console.log("No match");
-
-console.log("theNumbersMatch" +theNumbersMatch)
-
-var pens;
-
-var colorX =0;
-var colorY = 0;
-
-function getMouseCor(e){
-	// if (IE){
-	// 	colorX = event.clientX +document.body.scrollleft;
-	// 	colorY= event.clientY+document.body.scrollTop;
-	// }
-	// else{
-	var body=document.getElementsByTagName("body");
-	if (colorX<0) {colorX=0};
-	if (colorY<0){colorY=0};
-
-		colorX=e.pageX;
-		colorY=e.pageY;
-
-body[0].style.backgroundColor = "rgb(colorX,colorY,0)";
-	// }
-	
-	console.log(colorX);
-	console.log(colorY);
-	return true;
-}
-document.addEventListener("mouseover", getMouseCor);
-
+var a =9/1003;
+var b= 10/8081864;
+findBiggestFraction();
+console.log("Frist fraction result:", a)
