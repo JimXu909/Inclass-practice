be9329bcb193891fa3729eb80c59e3e1bdecd75f
JimXu <zxu01@risd.edu>
2019-01-20 17:09:22 -0500

    hw update

--- HW2/script.js
+++ HW2/script.js
@@ -1,4 +1,9 @@
-// alert(
-// 	"what ever the fuck i want")
+/*
+#1 listen to the mouse position event
+#2 get the coodinate and convert them into words
+#3 put the words inside the paragraphs
+#4 check if the paragraphs are full
+#5 if the paragraphs are full move it to the next paragraph
+*/
 
-document.querySelectorAll("p.sqbox").innerHTML="I'll do whatever the fuck I want";
+
